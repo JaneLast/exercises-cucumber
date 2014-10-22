7a47e384ee69010c1142118383ad6ba3eae5d214
JaneLast <jane.kempton@gmail.com>
2014-10-22 11:07:41 +0100

    Finished Chapter 7 exercise

---
+++ features/step_definitions/teller_steps.rb
@@ -0,0 +1,3 @@
+Given /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  teller.withdraw_from(my_account, amount)
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,7 @@
+#Env.rb
+
+require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'nice_bank')
+
 require 'rspec/expectations'
 require 'watir'
 require 'pry'
